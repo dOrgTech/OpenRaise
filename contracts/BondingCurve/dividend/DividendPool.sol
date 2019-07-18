6e999b77be1626b24dfdc7ab8b84a26a15c1e70b
tspoff@gmail.com <tspoff@gmail.com>
2019-07-18 16:55:10 -0400

    add dividend pool and test scaffold

--- contracts/BondingCurve/dividend/DividendPool.sol
+++ contracts/BondingCurve/dividend/DividendPool.sol
@@ -1,17 +1,21 @@
 pragma solidity >= 0.4.22 <6.0.0;
 
 import "zos-lib/contracts/Initializable.sol";
-// import "@statesauce/merkle-payments/contracts/PaymentPool.sol";
 import "openzeppelin-eth/contracts/token/ERC20/IERC20.sol";
+import "./PaymentPool.sol";
 
 /**
  * @title Dividend Payment Pool
  * @dev Allows withdrawals of allocated dividends by bonded token holders
  * Works with a single payment token
  */
-contract DividendPool is Initializable {
-    function initialize(IERC20 _paymentToken) public initializer {
-        // PaymentPool.initialize(_paymentToken);
+contract DividendPool is Initializable, PaymentPool {
+
+    /// @dev Initialize contract
+    /// @param paymentToken Token to recieve and track payments.
+    /// @param owner Contract owner. Has exclusive rights to upload new merkle roots.
+    function initialize(IERC20 paymentToken, address owner) public initializer {
+        PaymentPool.initialize(paymentToken, owner);
     }
 
 }
