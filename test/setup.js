7dc05a5dd42ef863ae3b265f380c9cc87023941a
tspoff@gmail.com <tspoff@gmail.com>
2019-08-18 20:46:20 +0200

    Add setup settings for coverage network

--- test/setup.js
+++ test/setup.js
@@ -5,9 +5,17 @@ import {ZWeb3, Contracts} from '@openzeppelin/upgrades';
 
 ZWeb3.initialize(web3.currentProvider);
 
+//TODO: Have these automatically set based on network chosen
+//Standard network defaults
+// Contracts.setArtifactsDefaults({
+//   gas: 6721975,
+//   gasPrice: 100000000000
+// });
+
+//Solidity coverage network defaults
 Contracts.setArtifactsDefaults({
-  gas: 6721975,
-  gasPrice: 100000000000
+  gas: 17592186044415,
+  gasPrice: 1
 });
 
 require('chai')
